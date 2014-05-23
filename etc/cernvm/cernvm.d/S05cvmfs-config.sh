b3142311943cc69ec00e77bb12783f6f5e6c0288
Jakob Blomer <jblomer@cern.ch>
2014-05-23 16:59:06 +0200

    adapt to new cvmfs config

--- etc/cernvm/cernvm.d/S05cvmfs-config.sh
+++ etc/cernvm/cernvm.d/S05cvmfs-config.sh
@@ -4,7 +4,12 @@
 
 cernvm_start () {
   if [ -d /etc/cvmfs/default.d ]; then
-    cat /etc/cernvm/default.conf /etc/cvmfs/site.conf /etc/cernvm/site.conf > /etc/cvmfs/default.d/75-cernvm.conf
+    cat /dev/null > /etc/cvmfs/default.d/75-cernvm.conf
+    for f in /etc/cernvm/default.conf /etc/cvmfs/site.conf /etc/cernvm/site.conf; do
+      if [ -f $f ]; then
+       cat $f >> /etc/cvmfs/default.d/75-cernvm.conf
+      fi
+    done
   fi
 }
 
