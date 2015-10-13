87bd378a5d76ca5847a9f5db0baa13418d46aff8
Jakob Blomer <jblomer@cern.ch>
2015-10-13 09:20:33 +0000

    update ssh conffigs

--- etc/cernvm/repositories.sh
+++ etc/cernvm/repositories.sh
@@ -1,8 +1,8 @@
 if [ "x$CERNVM_ORGANISATION" != "x" ]
 then
   cvmfs_groups=`echo $CERNVM_ORGANISATION | awk '{printf("%s",tolower($1))}' | sed -e 's/,/ /g'`
-  [ "$CERNVM_GRID_UI" == "on" ] && cvmfs_groups="$cvmfs_groups grid"
-  [ "$CERNVM_SFT" == "on" ] && cvmfs_groups="$cvmfs_groups sft"
+  [ "$CERNVM_GRID_UI" == "on" ] && cvmfs_groups="$cvmfs_groups grid.cern.ch"
+  [ "$CERNVM_SFT" == "on" ] && cvmfs_groups="$cvmfs_groups sft.cern.ch"
 fi
 
 CVMFS_REPOSITORIES=${CVMFS_REPOSITORIES:=$cvmfs_groups}
