afac6cc2638efdce00b89a5a4346e8e47acdf3e8
Jakob Blomer <jblomer@cern.ch>
2015-11-13 09:16:03 +0000

    fix cernvm init scripts

--- etc/cernvm/cernvm.d/S50sslcert.sh
+++ etc/cernvm/cernvm.d/S50sslcert.sh
@@ -1,13 +1,13 @@
 ###########################################################################
-# Install additional or update groups if needed
+# Create self-signed SSL certificate
 ###########################################################################
 
 cernvm_start () {
-  if [ -f /etc/pki/tls/private/localhost.key -o -f /etc/pki/tls/certs/localhost.crt ]; then
+  if [ -f /etc/pki/tls/private/localhost.key -a -f /etc/pki/tls/certs/localhost.crt ]; then
     return 0
   fi
 
-  if [ ! -f /etc/pki/tls/private/localhost.key ] ; then
+  if [ ! -f /etc/pki/tls/private/localhost.key ]; then
     /usr/bin/openssl genrsa -rand /proc/apm:/proc/cpuinfo:/proc/dma:/proc/filesystems:/proc/interrupts:/proc/ioports:/proc/pci:/proc/rtc:/proc/uptime 1024 > /etc/pki/tls/private/localhost.key 2> /dev/null
     chmod 0640 /etc/pki/tls/private/localhost.key
     chown root:apache /etc/pki/tls/private/localhost.key
@@ -18,7 +18,7 @@ cernvm_start () {
     FQDN=localhost.localdomain
   fi
 
-  if [ ! -f /etc/pki/tls/certs/localhost.crt ] ; then
+  if [ ! -f /etc/pki/tls/certs/localhost.crt ]; then
     cat << EOF | /usr/bin/openssl req -new -key /etc/pki/tls/private/localhost.key \
                    -x509 -days 365 -set_serial $RANDOM \
                    -out /etc/pki/tls/certs/localhost.crt 2>/dev/null
@@ -30,10 +30,11 @@ SomeOrganizationalUnit
 ${FQDN}
 root@${FQDN}
 EOF
-  chmod 444 /etc/pki/tls/certs/localhost.crt
+    chmod 444 /etc/pki/tls/certs/localhost.crt
   fi
 }
 
 cernvm_stop () {
   true
 }
+
