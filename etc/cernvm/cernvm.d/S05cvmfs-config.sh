34e16522a6cb04d579c3877b96712b0b13ab4b22
Jakob Blomer <jblomer@cern.ch>
2018-10-26 17:09:29 +0200

    fix auto pac/proxy with contextualization

--- etc/cernvm/cernvm.d/S05cvmfs-config.sh
+++ etc/cernvm/cernvm.d/S05cvmfs-config.sh
@@ -5,16 +5,27 @@
 cernvm_start () {
   # /etc/cvmfs/site.conf overwrites the GeoAPI proxy but can be overwritten by
   # amiconfig contextualization
+  local custom_pac_urls=0
+  local custom_http_proxies=0
   if [ -f /mnt/.rw/context/ucontext ]; then
     grep ^_UCONTEXT_CVMFS_HTTP_PROXY= /mnt/.rw/context/ucontext | sed s/^_UCONTEXT_// >> /etc/cvmfs/site.conf
     grep ^_UCONTEXT_CVMFS_PAC_URLS= /mnt/.rw/context/ucontext | sed s/^_UCONTEXT_// >> /etc/cvmfs/site.conf
-  else
-    local wpad_server_list="http://wlcg-wpad.cern.ch/wpad.dat http://wlcg-wpad.fnal.gov/wpad.dat"
-    local wpad_servers=$(echo $wpad_server_list | tr ' ' '\n' | shuf | tr '\n' ';' | sed 's/;$//')
-    echo "CVMFS_PAC_URLS=\"http://grid-wpad/wpad.dat;http://wpad/wpad.dat;${wpad_servers}\"" >> /etc/cvmfs/site.conf
-    echo "CVMFS_HTTP_PROXY=\"auto;DIRECT\"" >> /etc/cvmfs/site.conf
+    if grep -q ^_UCONTEXT_CVMFS_HTTP_PROXY= /mnt/.rw/context/ucontext; then
+      custom_http_proxies=1
+    fi
+    if grep -q ^_UCONTEXT_CVMFS_PAC_URLS= /mnt/.rw/context/ucontext; then
+      custom_pac_urls=1
+    fi
   fi
-  
+  local wpad_server_list="http://wlcg-wpad.cern.ch/wpad.dat http://wlcg-wpad.fnal.gov/wpad.dat"
+  local wpad_servers=$(echo $wpad_server_list | tr ' ' '\n' | shuf | tr '\n' ';' | sed 's/;$//')
+  if [ $custom_pac_urls -eq 0 ]; then
+    echo "CVMFS_PAC_URLS=\"http://grid-wpad/wpad.dat;http://wpad/wpad.dat;${wpad_servers}\"" >> /etc/cvmfs/site.conf
+  fi
+  if [ $custom_http_proxy -eq 0 ]; then
+    echo "CVMFS_HTTP_PROXY=\"auto;DIRECT\"" >> /etc/cvmfs/site.conf
+  fi 
+ 
   if [ -d /etc/cvmfs/default.d ]; then
     rm -f /etc/cvmfs/default.d/75-cernvm.conf
     ln -sf /etc/cernvm/default.conf /etc/cvmfs/default.d/75-cvmdefault.conf
